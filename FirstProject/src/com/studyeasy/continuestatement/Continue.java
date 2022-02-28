ac62f3567d127bd3c6f5c027a8c9a488c86dc0c8
JUSTIN SAMRAJ <92292875+JustinFSD@users.noreply.github.com>
2022-02-28 10:43:48 +0530

    Continue

---
+++ FirstProject/src/com/studyeasy/continuestatement/Continue.java
@@ -0,0 +1,15 @@
+package com.studyeasy.continuestatement;
+
+
+
+public class Continue {
+	public static void main(String[] args) {
+		for(int i=0; i<=20; i++) {
+			if(i%2==0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
