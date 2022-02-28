457f0169077fa355808538e3e5ce7cf9a37941fd
JUSTIN SAMRAJ <92292875+JustinFSD@users.noreply.github.com>
2022-02-28 09:59:43 +0530

    For1

---
+++ FirstProject/src/com/studyeasy/loop/For1.java
@@ -0,0 +1,17 @@
+package com.studyeasy.loop;
+
+public class For1 {
+	public static void main(String[] args) {
+		int[] a= new int[3];
+		
+		a[0] = 20;
+		a[1] = 30;
+		a[2] = 40;
+		
+		for(int i=0; i<=2; i=i+1) {
+		
+		System.out.println(a[i]);
+		}
+	}
+
+}
