aadbc6d8fb1c0bbbe348507ff669c99ae2b49ece
JUSTIN SAMRAJ <92292875+JustinFSD@users.noreply.github.com>
2022-02-28 10:29:48 +0530

    Switchcase

---
+++ FirstProject/src/com/studyeasy/switchcase/SwitchCase2.java
@@ -0,0 +1,19 @@
+package com.studyeasy.switchcase;
+
+public class SwitchCase2 {
+	public static void main(String[] args) {
+		String student = "Justin";
+		
+	switch(student) {
+	case "Sam":
+		System.out.println(student + " is from Bangalore");
+		break;
+	case "Justin":
+		System.out.println(student + " is from Chennai");
+		break;
+	case "Raj":
+		System.out.println(student + " is from Delhi");
+		}
+	}
+
+}
