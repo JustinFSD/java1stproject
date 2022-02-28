050785a8992cf56e960b6600406a2f43c935a0dd
JUSTIN SAMRAJ <92292875+JustinFSD@users.noreply.github.com>
2022-02-28 10:50:33 +0530

    Zoo

---
+++ FirstProject/src/com/studyeasy/construtor/ZooTester.java
@@ -0,0 +1,17 @@
+package com.studyeasy.construtor;
+
+
+public class ZooTester {
+	public static void main(String[] args) {
+		Zoo vandalur = new Zoo();
+		System.out.println(vandalur.name);
+		System.out.println(vandalur.noofanimals);
+		System.out.println(vandalur.zoosize + "sq.km");
+		System.out.println(vandalur.zooentryfees);
+		System.out.println(vandalur.iszooopeninsunday);
+		
+		
+	}
+	
+
+}
