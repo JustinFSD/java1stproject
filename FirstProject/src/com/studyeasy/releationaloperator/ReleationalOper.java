2cb5692649e8cd14a9901a99e755c8a12d0458ff
JUSTIN SAMRAJ <92292875+JustinFSD@users.noreply.github.com>
2022-02-28 11:02:54 +0530

    Releational Operator

---
+++ FirstProject/src/com/studyeasy/releationaloperator/ReleationalOper.java
@@ -0,0 +1,16 @@
+package com.studyeasy.releationaloperator;
+
+public class ReleationalOper {
+	public static void main(String[] args) {
+		int x=593;
+		int y=690;
+		
+		System.out.println(x==y);
+		System.out.println(x>=y);
+		System.out.println(x<=y);
+		System.out.println(x!=0);
+		System.out.println(x>10);
+		System.out.println(y<0);
+	}
+
+}
