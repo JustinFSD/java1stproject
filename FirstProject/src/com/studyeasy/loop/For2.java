f938807a022f2d2f6ebf5cf46443c39365a2c536
JUSTIN SAMRAJ <92292875+JustinFSD@users.noreply.github.com>
2022-02-28 10:05:09 +0530

    LOOP concept

---
+++ FirstProject/src/com/studyeasy/loop/For2.java
@@ -0,0 +1,14 @@
+package com.studyeasy.loop;
+
+public class For2 {
+	public static void main(String[] args) {
+		int[] y = new int[1000];
+				
+		for(int i = 0; i<=1000; i=i+1) {
+			y[i]= i;
+		
+			System.out.println("Justin =" + y[i]);
+		}
+	}
+
+}
---
+++ FirstProject/src/com/studyeasy/loop/For3.java
@@ -0,0 +1,14 @@
+package com.studyeasy.loop;
+
+public class For3 {
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for(int i=1; i<=100; i++) {
+			sum = sum+i;
+		}
+		
+		System.out.println("sum =" + sum);
+	}
+
+}
